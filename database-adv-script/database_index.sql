d9b07b63233ef6559a30171a5c61921576d5122f
Idoko Augustine <linkwithaugustine@gmail.com>
2025-09-06 18:33:39 +0100

    update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -11,5 +11,14 @@ CREATE INDEX idx_bookings_date ON Bookings(booking_date);
 CREATE INDEX idx_properties_location ON Properties(location);
 CREATE INDEX idx_properties_host_id ON Properties(host_id);
 
--- Example of composite index if queries often filter by multiple columns
+-- Example of composite index
 CREATE INDEX idx_bookings_user_property ON Bookings(user_id, property_id);
+
+-- Performance Check with EXPLAIN ANALYZE
+EXPLAIN ANALYZE
+SELECT u.first_name, u.last_name, b.booking_date, p.title
+FROM Users u
+JOIN Bookings b ON u.user_id = b.user_id
+JOIN Properties p ON b.property_id = p.property_id
+WHERE u.email = 'test@example.com'
+ORDER BY b.booking_date DESC;
