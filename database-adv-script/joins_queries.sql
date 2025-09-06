0f2b7c497267705540339ee052c755a7c126e65e
Idoko Augustine <linkwithaugustine@gmail.com>
2025-09-06 17:49:20 +0100

    update join_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -9,7 +9,8 @@ SELECT
     u.last_name,
     u.email
 FROM Booking b
-INNER JOIN User u ON b.user_id = u.user_id;
+INNER JOIN User u ON b.user_id = u.user_id
+ORDER BY b.start_date DESC;
 
 
 -- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
@@ -21,7 +22,8 @@ SELECT
     r.rating,
     r.comment
 FROM Property p
-LEFT JOIN Review r ON p.property_id = r.property_id;
+LEFT JOIN Review r ON p.property_id = r.property_id
+ORDER BY p.property_id, r.rating DESC;
 
 
 -- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
@@ -34,4 +36,5 @@ SELECT
     b.start_date,
     b.end_date
 FROM User u
-FULL OUTER JOIN Booking b ON u.user_id = b.user_id;
+FULL OUTER JOIN Booking b ON u.user_id = b.user_id
+ORDER BY u.user_id, b.start_date;
